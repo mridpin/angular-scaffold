718f990cfedf4caf8a4b7514e35621544a48a7af
mridpin <mridao99@gmail.com>
2021-11-09 22:14:45 +0100

    renamed app

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/kenjo-webapp'),
+      dir: require('path').join(__dirname, './coverage/angular-scaffold'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'kenjo-webapp'`, () => {
+  it(`should have as title 'angular-scaffold'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('kenjo-webapp');
+    expect(app.title).toEqual('angular-scaffold');
   });
 
   // it('should render title', () => {
   //   const fixture = TestBed.createComponent(AppComponent);
   //   fixture.detectChanges();
   //   const compiled = fixture.nativeElement;
-  //   expect(compiled.querySelector('.content span').textContent).toContain('kenjo-webapp app is running!');
+  //   expect(compiled.querySelector('.content span').textContent).toContain('angular-scaffold app is running!');
   // });
 });
