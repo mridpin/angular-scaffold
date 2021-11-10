6ee2fbc05f285444671551dbe4ef9371b4aedd70
mridpin <mridao99@gmail.com>
2021-11-10 23:07:52 +0100

    add request for artistcomponent

--- src/app/home/artists/artist/artist.component.ts
+++ src/app/home/artists/artist/artist.component.ts
@@ -1,7 +1,12 @@
 import { formatDate } from '@angular/common';
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 import { Artist } from 'src/app/core/models/artist';
+import { ArtistsAPIService } from 'src/app/core/services/api/artistsAPI.service';
+import { ArtistService } from 'src/app/core/services/artist.service';
 
 type ErrorDict = {
   [key: string]: boolean;
@@ -15,7 +20,9 @@ const minDate = new Date('1909-01-01T00:00:00.000Z');
   templateUrl: './artist.component.html',
   styleUrls: ['./artist.component.css']
 })
-export class ArtistComponent implements OnInit {
+export class ArtistComponent implements OnInit, OnDestroy {
+
+  private routeSub: Subscription = new Subscription();
 
   @Input() apiErrorMessage: string;
   // instead of input, we get from db, using route id, or even a resolver
@@ -47,27 +54,26 @@ export class ArtistComponent implements OnInit {
     return maxDate;
   }
 
-  constructor() {
+  constructor(private route: ActivatedRoute, private artistAPIService: ArtistsAPIService, private artistSrevice: ArtistService) {
     this.apiErrorMessage = '';
   }
 
   ngOnInit(): void {
-    // Artist is not undefined, we are editing
-    if (this.artist) {
-      this.formGroup.controls.name.setValue(this.artist.name);
+    this.route.params.pipe(
+      switchMap((params: Params) => this.artistAPIService.getArtist(params.id))
+    ).subscribe((artist: Artist) => {
+      this.artist = artist;
+      this.formGroup.controls.name.setValue(artist.name);
       this.formGroup.controls.birthdate.setValue(
-        formatDate(this.artist.birthdate, 'yyyy-MM-dd', 'en')
+        formatDate(artist.birthdate, 'yyyy-MM-dd', 'en')
       );
       this.formGroup.controls.deathDate.setValue(
-        this.artist.deathDate
-          ? formatDate(this.artist.deathDate, 'yyyy-MM-dd', 'en')
+        artist.deathDate
+          ? formatDate(artist.deathDate, 'yyyy-MM-dd', 'en')
           : ''
       );
-      this.formGroup.controls.photoUrl.setValue(this.artist.photoUrl);
-    } else {
-      // Artist is undefined, we are creating
-      // pass
-    }
+      this.formGroup.controls.photoUrl.setValue(artist.photoUrl);
+    });
   }
 
   saveEdit(): void {
@@ -94,6 +100,10 @@ export class ArtistComponent implements OnInit {
 
   closeErrorPanel(): void {
     this.clearErrorMessageEvent.emit();
+  }
+
+  ngOnDestroy(): void {
+    this.routeSub.unsubscribe();
   }
 
 }
